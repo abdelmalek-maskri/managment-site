facf7538622105502bdb21b5737ac5b3791ad9ed
Abdelmalek Maskri <abdelmalekmaskri18@gmail.com>
2024-09-18 19:23:49 +0100

    added some custom error messages for react-select and set the minimum date that the user can choose

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,15 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Create.css'
+import Select from 'react-select'
+import {useCollection} from '../../hooks/useCollection'
+
+
 export default function Create() {
+  const { document } = useCollection('users')
+  const [users, setUsers] = useState([])
+
+
   const [name, setName] = useState('')
   const [details, setDetails] = useState('')
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
+
+  const categories =[
+    {value: 'development', label: 'Development'},
+    {value: 'design', label: 'Design'},
+    {value: 'marketing', label: 'Marketing'},
+    {value: 'sales', label: 'Sales'},
+  ]
+
+  useEffect(() => {
+    if(document){
+      const options = document.map((user) => {
+        return { value: user , label : user.displayName}
+      })
+
+      setUsers(options)
+    }
+  }, [document])
+
   
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('name:', name, 'details:', details, 'dueDate:', dueDate)
+    setFormError(null)
+    //validate form
+
+    if(!category)
+      return setFormError('Please select a category')
+    if(assignedUsers.length === 0){
+      return setFormError('Please assign the project to at least one user')
+    }
+
+    console.log('name:', name, 'details:', details, 'dueDate:', dueDate, 'category:', category.value, 'assignedUsers:', assignedUsers)
 
   }
   
@@ -44,18 +80,26 @@ export default function Create() {
             placeholder='Due Date' 
             value={dueDate} 
             onChange={(e) => setDueDate(e.target.value)} 
+            min={new Date().toISOString().split('T')[0]}
             required 
           />
         </label>
         <label>
           <span>Project category:</span>
-          {/* {category select here} */}
+          <Select 
+            options={categories}
+            onChange={(option) => setCategory(option)}
+          />
         </label>
         <label>
           <span>Assign to:</span>
-          {/* {users select here} */}
+          <Select 
+            options={users}
+            onChange={(option) => setAssignedUsers(option)}
+            isMulti
+          />
         </label>
-
+        {formError && <div className='error'>{formError}</div>}
         <button className='btn'>Add Project</button>
       </form>
     </div>
