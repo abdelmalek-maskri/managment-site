0130a7bd27d509bedb35260c29e707173a8ece3d
Abdelmalek Maskri <abdelmalekmaskri18@gmail.com>
2024-09-20 19:31:37 +0100

    edited the projects page

--- src/pages/project/ProjectSummary.jsx
+++ src/pages/project/ProjectSummary.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Avatar from '../../components/Avatar'
 
 export default function ProjectSummary({project}) {
   return (
@@ -6,8 +7,20 @@ export default function ProjectSummary({project}) {
       <div className="project-summary">
         <h2 className='project-title'>{project.name}</h2>
         <p className='due-date'>
-            
+            project due by {project.dueDate.toDate().toDateString()}
         </p>
+        <p className='details'>
+            {project.details}
+        </p>
+        <h4>Project Assigned to:</h4>
+        <div className='assigned-users'>
+        {project.assignedUsersList.map(user => (
+            <div key={user.id}>
+                <Avatar src={user.photoURL} alt={user.displayName} className='avatar'/>
+            </div>
+        )
+        )}
+        </div>
       </div>
     </div>
   )
