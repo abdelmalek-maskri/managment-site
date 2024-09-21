6942a8ed4bcaf1757d280f012b09f7e0dc1d428e
Abdelmalek Maskri <abdelmalekmaskri18@gmail.com>
2024-09-21 18:18:44 +0100

    styled the createdBy paragraph

--- src/pages/project/ProjectSummary.jsx
+++ src/pages/project/ProjectSummary.jsx
@@ -17,7 +17,7 @@ export default function ProjectSummary({project}) {
     <div>
       <div className="project-summary">
         <h2 className='project-title'>{project.name}</h2>
-        <p>By {project.createdBy.displayName}</p>
+        <p className='created-by'>By {project.createdBy.displayName}</p>
 
         <p className='due-date'>
             project due by {project.dueDate.toDate().toDateString()}
